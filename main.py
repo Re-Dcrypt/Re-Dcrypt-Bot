7907d9c0c31266a0d9d8bc1a24ab402055e36a89
Warhawk947 <50366676+Warhawk947@users.noreply.github.com>
2021-02-14 10:47:22 -0500

    fixed some padding

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ class MyClient(discord.Client):
             decrypt=Cipher.Caesar.decode(encrypt, key).replace('|',' ')
             await message.channel.send(f"`{decrypt}`")
 
+         
+            
+            
         if message.content.startswith('&?b64_decode'):
             encrypt=message.content.split()
             '''
@@ -51,13 +54,19 @@ class MyClient(discord.Client):
             decrypt = base64.b64decode(encrypt).decode('utf-8')
             await message.channel.send(f"`{decrypt}`")
         
+        
+        
+        
+        
         if message.content.startswith('&?b64_encode'):
             to_encrypt = ' '.join(message.content.split()[1:])
             message_bytes = to_encrypt.encode('ascii')
             base64_bytes = base64.b64encode(message_bytes)
             base64_message = base64_bytes.decode('ascii')
-            
             await message.channel.send(f'`{base64_message}`')
+        
+        
+        
 
         if message.content.startswith('&?a1z26'):
             alphabets=list(string.ascii_lowercase)
@@ -78,10 +87,12 @@ class MyClient(discord.Client):
 
                 for i in num_list:
                     decrypt=decrypt + " " + str(alphabets[i])
-            
-            
             await message.channel.send(f"`{decrypt}`")
 
+            
+            
+            
+         
         if message.content.startswith('&?atbash'):
             alphabets=list(string.ascii_lowercase)
             cont=message.content.split()
@@ -97,9 +108,11 @@ class MyClient(discord.Client):
                     decrypt=decrypt+(alphabets[25-alphabets.index(i.lower())])
                 except:
                     decrypt=decrypt+str(i)
-                    
             await message.channel.send(f"`{decrypt}`")
 
+            
+            
+            
         if message.content.startswith('&?feedback'):
             cont=message.content.replace('&?feedback ','')
             channel=client.get_channel(810404758706716672)
@@ -110,9 +123,12 @@ class MyClient(discord.Client):
             emoji2='\N{THUMBS DOWN SIGN}'
             await mess.add_reaction(emoji1)
             await mess.add_reaction(emoji2)
-
             #await channel.send(f'{message.author} sent a feedback: {cont}')
             await message.channel.send('Feedback sent')
+            
+            
+            
+            
             
         if message.content.startswith('&?help'):
             embed=discord.Embed(title="Re-Dcrypt Bot Help", description="Re-Dcrypt Bot is a bot that will help you decode many ciphers like caesar , base64, A1Z26 & Atbash.", color=0x39ff14)
@@ -134,6 +150,9 @@ class MyClient(discord.Client):
             embed2.set_footer(text="Re-dcrypt Bot v0.5 Beta")
             await message.channel.send(embed=embed)
             await message.channel.send(embed=embed2)
+            
+            
+            
             
         if message.content.startswith('&?invite'):
             await message.channel.send('https://discord.com/api/oauth2/authorize?client_id=775629409494630410&permissions=67451968&scope=bot')
